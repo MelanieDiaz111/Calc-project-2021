904bf506129d47b517bcc6331944c6053a5541d9
MelanieDiaz111 <melanie.diaz1027@gmail.com>
2021-07-08 14:24:38 +0000

    Added while loop

---
+++ calc.py
@@ -0,0 +1,14 @@
+# Great the user
+print ("Hello, I'm the Fortune Robot.")
+# Ask the user if they want their future told
+answer = input("Do you want a Fortune? ")
+
+while answer.lower() != "no":
+    print("User asked for a fortune")
+    answer = input("Do you want a Fortune? ")
+
+# Fortune given
+
+# Ask if user wants another fortune
+
+# Repeat until user quits
